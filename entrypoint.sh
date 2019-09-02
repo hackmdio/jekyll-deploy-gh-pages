ab621315a8cf1e13c0cf487c1c5c67779ae58298
Max Wu <jackymaxj@gmail.com>
2019-09-02 23:49:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,14 @@ echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO GITHUB-PAGES'
 cd build
 remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 remote_branch="gh-pages" && \
-git clone $remote_repo --branch $remote_branch && \
-git checkout $remote_branch && \
+git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l && \
 git commit -m'action build' > /dev/null 2>&1 && \
-git push --force $remote_repo $remote_branch > /dev/null 2>&1 && \
+git push --force $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
-cd ../
+cd ../ && \
+curl -H "Accept: application/vnd.github.mister-fantastic-preview+json" -X POST https://api.github.com/repos/${GITHUB_REPOSITORY}/pages/builds
 echo '👍 GREAT SUCCESS!'
